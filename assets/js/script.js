671095470eacdf4b9ed111299cfc6177734a4291
lilynamou <lilyananamou@gmail.com>
2022-04-07 21:13:13 -0700

    Updating JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
 //Today's time and Date
 var currentDate = moment().format('MMMM Do YYY, h:mm:ss a');
-var currentDay = moment().format('dddd');
+
+var DayOfWeek = moment().format('dddd');
 console.log();
+
+//Adding Save Btn
+$("button").click(function() {
+    $('p').html()
+});
+
+
+//Retrieve local storage data
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
