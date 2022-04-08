575e4fa0e495150d728da6cd347f1146e4e1214e
lilynamou <lilyananamou@gmail.com>
2022-04-07 23:47:21 -0700

    updating JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,41 @@
-//Today's time and Date
-$("#currentDay").text(moment().format('dddd, MMMM Do YYYY'));
+//Shows weekday today and today's Date
+var currentDay = moment().format('dddd, MMMM Do YYYY'));
+$("#currentDay").html(currentDate);
+
+$(document).ready(function () {
+    //CLick listnr save bttn:
+    $(document).ready(function() {
+
+    $(".saveBtn").on("click", function () {
+
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+})
+
+
+function timeTracker() {
+//Current # of Hours
+    var currentHour = moment().hour(); 
+//
+    $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split('hour')[1]);
+    })
+
+};
+//checking if passed time
+if (blockHour < currentHour) {
+    timeContainer.addClass("past");
+    timeContainer.removeClass("future");
+    timeContainer.removeClass("present");
+}
+else if (blockHour === currentHour) {
+    timeContainer.addClass("present");
+}
+else {
+    timeContainer.addClass("future");
+}
+})
+}
 
 //Retrieve local storage data
 $("#hour9 .description").val(localStorage.getItem("hour9"));
